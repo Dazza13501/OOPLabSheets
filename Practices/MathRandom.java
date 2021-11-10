a7540e5cfb98a7677ef1eaa1abf98e109b6c4831
T00211191 <Darragh.Leahy@ittralee.ie>
2021-11-10 10:55:32 +0000

    10/11/21

--- Practices/MathRandom.java
+++ Practices/MathRandom.java
@@ -7,7 +7,7 @@ public class MathRandom {
 
 
         int num = (int) (Math.random() * 101);
-        //math.random() default is 0 to 1.To get 0 to 100, multiply by 101. 0-10, multiply by 11 etc. Multiply by 1 no. above for range.
+        //math.random() default is 0 to 1.To get 0 to 100, multiply by 101. 0-10, multiply by 11 etc. Multiply by 1 no. above for range. To exclude 0. multiply by 100 then add 1 for range 1-100.
 
        if(num<50)
            JOptionPane.showMessageDialog(null, num+" is less than 50. A number less than 50 was generated.");
