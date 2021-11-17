c7016e12e04bd595fdf7e5042786e0f5bf8da02b
T00211191 <Darragh.J.Leahy@students.ittralee.ie>
2021-11-17 10:55:18 +0000

    17/11/21

--- MiniProject/GameStore.java
+++ MiniProject/GameStore.java
@@ -1,4 +1,45 @@
 package MiniProject;
 
+import javax.swing.*;
+import java.util.GregorianCalendar;
+
 public class GameStore {
+
+    public static void main(String[] args) {
+
+        String output="";
+
+        Game g1 = new Game();
+        output+= "\n Value of first Game is: "+g1;
+
+        output+= "\n Calling Game multi constructor.";
+
+        String category = JOptionPane.showInputDialog("Please enter the category of the game ");
+        String name = JOptionPane.showInputDialog("Please enter the name of the game ");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Please enter the age group of the game "));
+        double price = Double.parseDouble(JOptionPane.showInputDialog("Please enter the price of the game "));
+
+        Game g2 = new Game(category, name, age, price);
+        output+= "\n Value of second Game is: "+g2;
+
+        Staff s1 = new Staff();
+        output+= "\n Value of first Staff is: "+s1;
+
+        output+= "\n Calling Staff multi constructor. ";
+         String forename = JOptionPane.showInputDialog("Please enter your forename");
+         String surname = JOptionPane.showInputDialog("Please enter your surname");
+         String gender = JOptionPane.showInputDialog("Please enter your gender");
+         GregorianCalendar DOB = GregorianCalendar.thisGregorianCalendar(JOptionPane.showInputDialog("Please enter your DOB in the format dd/mm/yyyy"));
+         String address = JOptionPane.showInputDialog("Please enter your address");
+         int PhoneNo = Integer.parseInt(JOptionPane.showInputDialog("Please enter your phone number"));
+        output+= "\n Value of first Game is: "+g1;
+        output+= "\n Value of first Game is: "+g1;
+        output+= "\n Value of first Game is: "+g1;
+        output+= "\n Value of first Game is: "+g1;
+
+
+
+
+
+    }
 }
--- MiniProject/Sales.java
+++ MiniProject/Sales.java
@@ -1,8 +1,59 @@
 package MiniProject;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Sales {
 
-    private Date salesDate;
+    private String salesDate;
+    private double sales = 0;
+    private double totalSales = 0;
+
+
+    public Sales() {
+        this(null, 0,0);
+    }
+
+
+    public Sales(String salesDate, double sales, double totalSales) {
+        setSalesDate(salesDate);
+        setSales(sales);
+        setTotalSales(totalSales);
+    }
+
+    public String getSalesDate() {
+        Date salesDate = new Date();
+        SimpleDateFormat format =new SimpleDateFormat("dd/MM/yy");
+        String date = format.format(salesDate);
+        return date;
+    }
+
+    public void setSalesDate(String salesDate) {
+        this.salesDate = salesDate;
+    }
+
+    public double getSales() {
+        sales += sales;
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public double getTotalSales() {
+        totalSales += sales;
+        return totalSales;
+    }
+
+    public void setTotalSales(double totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public String toString(){
+
+        return "Sales: "+getSales();
+
+    }
 }
